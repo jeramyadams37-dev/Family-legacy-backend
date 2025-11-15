c378a9a916902ce7c33736c3f65f61403c605202
jeramyadams37-dev <jeramyadams37@gmail.com>
2025-11-15 12:03:58 -0600

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,21 @@
+{
+  "name": "family-legacy-api",
+  "version": "1.0.0",
+  "description": "Family Legacy API Backend",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "pg": "^8.11.3",
+    "cors": "^2.8.5"
+  },
+  "engines": {
+    "node": ">=18.0.0"
+  },
+  "keywords": ["family", "legacy", "api"],
+  "author": "",
+  "license": "ISC"
+}
